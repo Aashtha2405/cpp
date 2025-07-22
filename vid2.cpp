1ad285aa75cb4126df1648cfecdd435f70b7e334
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-23 00:17:58 +0530

    Add files via upload

---
+++ vid2.cpp
@@ -0,0 +1,7 @@
+// basics of c++ and meaning of  code
+#include<iostream> // this is called header file which performs input and output operations.
+int main(){ //main function where excution starts
+    std::cout<<"Hello Aastha"; // std::cout is used to print output to the cvonsole.
+    return 0; // successful termination of program.
+    
+}
