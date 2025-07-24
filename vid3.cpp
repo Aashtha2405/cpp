87833ce1aeb4dc263292fa935156297ce8ed2161
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-24 19:36:32 +0530

    Add files via upload

---
+++ vid3.cpp
@@ -0,0 +1,34 @@
+// variable and comments
+// variables are containers to store our data
+
+#include<iostream>
+using namespace std;
+// this program is created byy me'
+int main(){
+    cout<< "Hello Aastha";
+    return 0;
+
+}
+
+
+
+
+// variables are datatypes that are used to store data in a program these are float,int and char
+//float for decimal,int for integers and char for characters
+
+
+
+/*this is multi line comments
+aastha singh 
+rajput
+hello 
+*/
+ 
+
+#include<iostream>
+using namespace std;
+int main(){
+    char name = 'a';
+    cout<<"hello" <<name;
+    return 0;
+}
